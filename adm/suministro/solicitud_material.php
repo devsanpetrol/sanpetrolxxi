243541940d8eafe3b1652e45bd7bc4211f594f01
devsanpetrol <gerardo.hernandez@sanpetrol.mx>
2020-09-21 16:57:27 -0500

    Almacen App...2020-09-21 3

--- adm/suministro/solicitud_material.php
+++ adm/suministro/solicitud_material.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="images/favicon.ico" type="image/ico" />
 
-    <title> SANPETROL </title>
+    <title><?php include '../../bar_nav/title.php'; ?></title>
 
     <!-- Bootstrap -->
     <link href="../../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
